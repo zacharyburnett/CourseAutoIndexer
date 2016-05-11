1a2218db282b8feb4f4b10e39dec20066ca3dad4
Zachary Burnett <zrb@umd.edu>
2016-05-11 13:50:41 -0400

    Changed directory structure for better course system

--- CourseAutoIndexer/CourseAutoIndexer.py
+++ CourseAutoIndexer/CourseAutoIndexer.py
@@ -3,16 +3,16 @@ Created on May 5, 2016
 
 @author: Zach
 '''
-import requests, time, datetime, os, sys
+import requests, datetime, os
 
 default_root_url = "https://ntst.umd.edu/soc"
 semesters = {"Spring": "01", "Summer": "05", "Fall": "08", "Winter": "12"}
 years = range(2014, datetime.datetime.now().year + 1)
-root_directory = "C:/Users/Zach/Desktop/courses"
+root_dir = "C:/Users/Zach/Desktop/courses"
 
 def get_semester(month):
     """Returns semester of given month number."""
-    for semester in semesters:
+    for semester in sorted(semesters):
         if month >= int(semesters[semester]):
             return semester
         
@@ -110,17 +110,16 @@ def get_majors(root_url=default_root_url):
 
 def parse_courses(semester=current_semester, year=current_year, root_url=default_root_url):
     """Returns course data in CSV format for given semester and year"""
-    then = time.time()
     print("Downloading course data for " + semester + " " + str(year) + " semester...")
     
     majors = get_majors(root_url)
     
-    courses_csv = ",".join(("Course ID", "Title", "Major", "Credits", "Grading Methods", "GenEd", "Prerequisites", "Restrictions", "Equivalences", "Description")) + "\n"
+    semester_csv = ",".join(("Course ID", "Title", "Major", "Credits", "Grading Methods", "GenEd", "Prerequisites", "Restrictions", "Equivalences", "Description")) + "\n"
     
     for major in majors:
         major_url = root_url + "/" + str(year) + semesters[semester] + "/" + major
         # get part of HTML relevant to course info and split into courses
-        major_html = requests.get(major_url).text.partition('<div class="courses-container">')[2].partition('<script type="text/javascript">')[0].split('<div id="')
+        major_html = requests.get(major_url).text.partition('<div class="courses-container">')[2].partition('<script type="text/javascript">')[0].split('<div id="' + major)
         del major_html[0]
         
         for course_html in major_html:
@@ -137,10 +136,9 @@ def parse_courses(semester=current_semester, year=current_year, root_url=default
             prerequisites, restrictions, equivalences, description = parse_course_text(parse_between(course_html, '<div class="approved-course-text">', '</div>'))
             
             # put into CSV sanitized format
-            courses_csv += '"' + '","'.join((course_id, course_title, major, course_min_credits, grading_methods, gen_ed_codes, prerequisites, restrictions, equivalences, description)) + '"' + "\n"
+            semester_csv += '"' + '","'.join((course_id, course_title, major, course_min_credits, grading_methods, gen_ed_codes, prerequisites, restrictions, equivalences, description)) + '"' + "\n"
         
-    print("Download complete, took " + str(int(time.time() - then)) + " seconds.")
-    return courses_csv
+    return semester_csv
 
 class Course:
     """Course with major, course_id, course_title, credits, grading_methods methods, GenEd gen_ed_codes, prerequisites, and description."""
@@ -155,66 +153,90 @@ class Course:
         self.restrictions = restrictions
         self.equivalences = equivalences.split(";")
         self.description = description
+        self.years = {}
     
     def __str__(self):
-        return self.course_id + " " + self.course_title + "\n" + "Major: " + self.major + "\n" + "Credits: " + self.min_credits + "\n" + "Grading methods: " + str(self.grading_methods) + "\n" + "GenEd: " + str(self.gen_ed_codes) + "\n" + "Prerequisites: " + str(self.prerequisites) + "\n" + "Restrictions: " + str(self.restrictions) + "\n" + "Equivalences: " + str(self.equivalences) + "\n" + "Description: " + self.description
-    
-def get_courses(semester, year, data_file):
-    """Returns list of Course objects parsed from main page"""
-    if not os.path.isfile(data_file):
-        print("Data file " + data_file + " does not exist.")
-        ensure_dir(data_file.rsplit("/", 1)[0])
-        parse_courses_to_file(data_file, semester=semester, year=year)
-    else:
-        print("Data file " + data_file + " already exists.")
-        
-    #input_csv = parse_courses(semester=semester, year=year).split("\n")
-    input_csv = open(data_file, "r").readlines()
-    #columns = input_csv[0].strip("\n").split(",")
-    del input_csv[0]
-    del input_csv[len(input_csv) - 1]
-    
-    courses = []
-    for line in input_csv:
-        values = line.strip("\n").strip('"').split('","')
-        course_id, course_title, major, course_min_credits, grading_methods, course_subcategory, prerequisites, restrictions, equivalences, description = values
-        course = Course(course_id, course_title, major, course_min_credits, grading_methods, course_subcategory, prerequisites, restrictions, equivalences, description)
-        courses.append(course)
-    
-    return courses
-
-def write_index_html(course_id, course_title, body_preformatted, path):
-    ensure_dir(path)
-    index_html = open(path + "/index.html", "w")
-    parent = path.rsplit("/", 1)[0].partition("www")[2]
-    header = course_id + ": " + course_title
-    
-    print("<!DOCTYPE=html><html>", file=index_html)
-    print("<title>" + header + "</title>", file=index_html)
-    print("<h1>" + header + "</h1>", file=index_html)
-    print('<p><a href="' + parent + '">' + "Back to " + parent.rsplit("/", 1)[1] + "</a></p>", file=index_html)
-    print("<body>", file=index_html)
-    print("<p>", file=index_html)
-    print("<pre>", file=index_html)
-    print(body_preformatted, file=index_html)
-    print("</pre>", file=index_html)
-    print("</p>", file=index_html)
-    print("<br>" + course_id + " OurUMD page<br>", file=index_html)
-    print('<iframe src="http://www.ourumd.com/class/' + course_id + '" height="50%" width="100%"></iframe>', file=index_html)
-    print("</body>", file=index_html)
-    print("</html>", file=index_html)
+        return self.major + self.course_id + " " + self.course_title + "\n" + "Major: " + self.major + "\n" + "Credits: " + self.min_credits + "\n" + "Grading methods: " + str(self.grading_methods) + "\n" + "GenEd: " + str(self.gen_ed_codes) + "\n" + "Prerequisites: " + str(self.prerequisites) + "\n" + "Restrictions: " + str(self.restrictions) + "\n" + "Equivalences: " + str(self.equivalences) + "\n" + "Description: " + self.description
 
 years = range(2014, datetime.datetime.now().year + 1)
-root_directory = "C:/WAMP/www/courses"
+root_dir = "C:/Users/Zach/Desktop/courses"
+courses = {}
 
 def ensure_dir(path):
     if not os.path.isdir(path):
         os.makedirs(path)
 
 for year in years:
-    year_directory = root_directory + "/" + str(year)
-    for semester in semesters:
-        semester_directory = year_directory + "/" + semester
-        print("Writing HTML for " + semester + " " + str(year) + " semester...")
-        for course in get_courses(semester, year, semester_directory + "/courses_" + semester + "_" + str(year) + ".csv"):                      
-            write_index_html(course.course_id, course.course_title, str(course), semester_directory + "/" + course.course_id)
+    for semester in sorted(semesters):
+        semester_csv = root_dir + "/semesters/" + str(year) + "_" + str(semesters[semester]) + "_" + semester + ".csv"
+        if not os.path.isfile(semester_csv):
+            ensure_dir(semester_csv.rsplit("/", 1)[0])
+            parse_courses_to_file(semester_csv, semester=semester, year=year)
+        else:
+            print("Data file " + semester_csv + " already exists.")
+        
+        data = open(semester_csv, "r").readlines()
+        del data[0]
+        del data[len(data) - 1]
+                
+        for line in data:
+            values = line.strip("\n").strip('"').split('","')
+            course_id, course_title, major, course_min_credits, grading_methods, course_subcategory, prerequisites, restrictions, equivalences, description = values
+            course = Course(course_id, course_title, major, course_min_credits, grading_methods, course_subcategory, prerequisites, restrictions, equivalences, description)
+            
+            name = major + course_id
+            
+            if name not in courses:
+                courses[name] = course
+                
+            if year not in courses[name].years:
+                courses[name].years[year] = []
+
+            courses[name].years[year].append(semester)
+
+courses_html = open(root_dir + "/index.html", "w")
+print("<!DOCTYPE=html>\n<html>", file=courses_html)
+print("<title>Courses</title>\n<body>", file=courses_html)
+print("<h1>Courses</h1>", file=courses_html)
+print('<p>\n<a href="/">Back to home.</a>\n</p>', file=courses_html)
+print('<p>\n<a href="semesters">Semester data.</a>\n</p>', file=courses_html)
+
+print("<body>\n<p>", file=courses_html)
+
+for course in sorted(courses):
+    major = courses[course].major
+    course_id = courses[course].course_id
+    course_title = courses[course].course_title
+    
+    path = root_dir +  "/" + major + "/" + course_id
+    ensure_dir(path)
+    course_html = open(path + "/index.html", "w")
+    header = course_id + ": " + course_title
+    
+    print('<a href="' + path + '">' + major + course_id + ': ' + course_title + '</a><br>', file=courses_html, end="\n")
+    
+    print("<!DOCTYPE=html>\n<html>", file=course_html)
+    print("<title>" + header + "</title>", file=course_html)
+    
+    print("<h1>" + header + "</h1>", file=course_html)
+        
+    print('<p>\n<a href="/courses">' + "Back to courses</a>\n</p>", file=course_html)
+    
+    print("<body>\n<p>\n<pre>\n" + str(courses[course]) + "\n</p>", file=course_html)
+
+    print("<p>", file=course_html)
+    for year in sorted(courses[course].years):
+        print(year, file=course_html)
+        for semester in courses[course].years[year]:
+            print(semester, file=course_html)
+    print("</pre></p>", file=course_html, end="\n\n")
+      
+    print("<br>" + course_id + " OurUMD page<br>", file=course_html)
+    print('<iframe src="http://www.ourumd.com/class/' + major + course_id + '" height="50%" width="100%"></iframe>', file=course_html)
+    
+    print("</body>\n</html>", file=course_html)
+    
+    course_html.close()
+    
+print("</p>\n</body>\n</html>", file=courses_html)
+courses_html.close()
